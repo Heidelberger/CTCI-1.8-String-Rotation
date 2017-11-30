cb7b6deef1786d845934e2105102af6a83db6d45
Qubert <upafourth@gmail.com>
2017-11-30 11:13:27 +0100

    Add project files.

---
+++ CTCI 1.8 String Rotation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI_1._8_String_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
